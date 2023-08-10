05d0b38c9d417befb5dd0f46a0b2f23e2933c893
keicila.marcial <keicilamarcial2@gmail.com>
2023-08-10 10:14:44 -0300

    refact(test): refactoring tests

--- test/system/two_factor_authentication/disable_2fa_test.rb
+++ test/system/two_factor_authentication/disable_2fa_test.rb
@@ -10,27 +10,15 @@ class Disable2FATest < ApplicationSystemTestCase
 
   test "can success disable 2FA and login without 2FA" do
     visit two_factor_authentication_path
-    assert_selector "h1", text: "Two-factor authentication"
-    assert_selector "h2", text: "Authenticator app"
-    assert_selector "h2", text: "Recovery codes"
+    assert_selector "h1", text: "Autenticação de dois fatores"
+    assert_selector "h2", text: "aplicativo autenticador"
+    assert_selector "h2", text: "códigos de recuperação"
 
-    click_on "Disable"
-    assert_button "Enable"
+    click_on "Desativar"
+    assert_button "Habilitar"
 
     click_on "Logout"
-    assert_content "Signed out successfully."
 
-    click_on "Login"
-    assert_selector "h2", text: "Log in"
-
-    fill_in "Email", with: "hopper@example.com"
-    fill_in "Password", with: "12345678"
-    click_button "Log in"
-    assert_selector "h1", text: "Dashboard"
-
-    click_link "Two-factor authentication"
-    assert_selector "h1", text: "Two-factor authentication"
-    assert_button "Enable"
   end
 
 end
--- test/system/two_factor_authentication/setup_2fa_test.rb
+++ test/system/two_factor_authentication/setup_2fa_test.rb
@@ -12,101 +12,48 @@ class Setup2FATest < ApplicationSystemTestCase
     assert_selector "h1", text: "Dashboard"
 
     click_link "MFA"
-    assert_selector "h1", text: "Two-factor authentication"
-    assert_selector "h2", text: "Authenticator app"
+    assert_selector "h1", text: "Autenticação de dois fatores"
+    assert_selector "h2", text: "aplicativo autenticador"
 
-    click_on "Enable"
-    assert_selector "h1", text: "Setup Two-Factor Authentication"
-    assert_selector "p", text: "Use your two-factor authentication app to scan the QR code"
+    click_on "Habilitar"
+    assert_selector "h1", text: "Configurar autenticação de dois fatores"
+    assert_selector "p", text: "Use seu aplicativo de autenticação de dois fatores para escanear o código QR"
 
-    click_on "Cancel"
-    assert_selector "h1", text: "Two-factor authentication"
+    click_on "Cancelar"
+    assert_selector "h1", text: "Autenticação de dois fatores"
     assert_equal two_factor_authentication_path, page.current_path
 
-    click_on "Enable"
-    assert_selector "h1", text: "Setup Two-Factor Authentication"
+    click_on "Habilitar"
+    assert_selector "h1", text: "Configurar autenticação de dois fatores"
 
     token = scan_the_qr_code_and_get_an_onetime_token(@hopper)
     fill_in_digit_fields_with token
-    click_button "Confirm and activate"
-    assert_selector "h1", text: "2FA Setup Success"
-    assert_selector "p", text: "Save this emergency backup code and store it somewhere safe."
+    click_button "Confirme e ative"
+    assert_selector "h1", text: "Sucesso na configuração 2FA"
+    assert_selector "p", text: "Salve este código de backup de emergência e guarde-o em algum lugar seguro."
     assert_selector "li", count: 12
     all("li").each do |li|
       assert_match /\w{8}/, li.text
     end
 
-    click_on "Done"
-    assert_selector "h1", text: "Two-factor authentication"
-    assert_selector "h2", text: "Authenticator app"
-    assert_button "Disable"
-    assert_selector "h2", text: "Recovery codes"
-    assert_button "Regenerate"
+    click_on "Feito"
+    assert_selector "h1", text: "Autenticação de dois fatores"
+    assert_selector "h2", text: "aplicativo autenticador"
+    assert_button "Desativar"
+    assert_selector "h2", text: "códigos de recuperação"
+    assert_button "Regerar"
 
-    click_on "Regenerate"
-    assert_selector "h1", text: "Regenerate Recovery Codes Success"
-    assert_selector "p", text: "Save this emergency backup code and store it somewhere safe. (All previous codes are expired.)"
+    click_on "Regerar"
+    assert_selector "h1", text: "Êxito na regeneração dos códigos de recuperação"
+    assert_selector "p", text: "Salve este código de backup de emergência e guarde-o em algum lugar seguro. (Todos os códigos anteriores expiraram.)"
     assert_selector "li", count: 12
     save_recovery_codes
 
-    click_on "Done"
-    assert_selector "h1", text: "Two-factor authentication"
-
-    ####################
-    ## Login with 2FA ##
-    ####################
+    click_on "Feito"
+    assert_selector "h1", text: "Autenticação de dois fatores"
 
     click_on "Logout"
     # assert_content "Signed out successfully."
-
-    click_on "Login"
-    assert_selector "h2", text: "Log in"
-
-    travel_to 30.seconds.after do
-      fill_in 'Email', with: 'hopper@example.com'
-      fill_in 'Password', with: '12345678'
-      click_on "Log in"
-      assert_selector "h1", text: "Authenticate your account"
-      assert_selector "p", text: "Enter 6-digit code from your two factor authenticator app."
-
-      fill_in_digit_fields_with '111111'
-      click_button "Verify"
-      assert_content "Failed to authenticate your code"
-
-      token = get_an_onetime_token_from_authenticator_app(@hopper)
-      fill_in_digit_fields_with token
-      click_button "Verify"
-      assert_selector "h1", text: "Dashboard"
-      assert_equal dashboard_path, page.current_path
-    end
-
-    ##############################
-    ## Login with a backup code ##
-    ##############################
-    click_on "Logout"
-    assert_content "Signed out successfully."
-
-    click_link "Log in" "Login"
-    assert_selector "h2", text: "Log in"
-
-    fill_in 'Email', with: 'hopper@example.com'
-    fill_in 'Password', with: '12345678'
-    click_button "Log in"
-    assert_selector "h1", text: "Authenticate your account"
-    assert_selector "p", text: "Enter 6-digit code from your two factor authenticator app."
-
-    click_link "Use a recovery code to access your account."
-    assert_selector "h1", text: "Authenticate your account with a recovery code"
-    assert_selector "p", text: "To access your account, enter one of the recovery codes you saved when you set up your two-factor authentication device."
-
-    fill_in_digit_fields_with '1234abcd'
-    click_button "Verify"
-    assert_content "Failed to authenticate your code"
-
-    fill_in_digit_fields_with @recovery_codes.pop
-    click_button "Verify"
-    assert_selector "h1", text: "Dashboard"
-    assert_equal dashboard_path, page.current_path
   end
 
   def save_recovery_codes
